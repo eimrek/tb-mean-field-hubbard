6e692d60b8dba24df53dfb085a83d54378f8d5e2
Kristjan <kristjaneimre@gmail.com>
2021-05-04 21:35:50 +0200

    prepare release 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tb-mean-field-hubbard",
-    version="1.3.0",
+    version="1.3.1",
     author="Kristjan Eimre",
     author_email="kristjaneimre@gmail.com",
     description="Package to run tight-binding mean field hubbard calculations",
--- tb_mean_field_hubbard/__init__.py
+++ tb_mean_field_hubbard/__init__.py
@@ -1,3 +1,3 @@
 from .mfh import MeanFieldHubbardModel
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
