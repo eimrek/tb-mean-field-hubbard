f904bfe10984c8b46ee0e1de51524b35796493f4
Kristjan Eimre <kristjaneimre@gmail.com>
2022-06-25 22:25:40 +0300

    prepare release 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tb-mean-field-hubbard",
-    version="2.0.0",
+    version="2.0.1",
     author="Kristjan Eimre",
     author_email="kristjaneimre@gmail.com",
     description="Package to run tight-binding mean field hubbard calculations",
